c3bc467cf2930c8b69856f141161a2c73e2941ec
Novout <novout@hotmail.com>
2026-01-19 21:18:06 -0300

    chore: generi

--- generi.config.ts
+++ generi.config.ts
@@ -1,14 +1,14 @@
 export default {
-	repository: "https://github.com/Novout/nvton",
+	repository: 'https://github.com/ZEXagonal-mente/nvton',
 	silent: false,
-	commits: "conventional-commits",
+	commits: 'conventional-commits',
 	tag: true,
 	version: true,
 	push: true,
 	release: false,
 	publish: false,
-	exclude: [" typo"],
-	prerelease: "beta",
-	packagePath: "package.json",
-	lernaPath: "lerna.json"
-}
+	exclude: [' typo'],
+	prerelease: 'beta',
+	packagePath: 'package.json',
+	lernaPath: 'lerna.json',
+};
